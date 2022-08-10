7029eaaa762c560607ceb78f462631fb024b912f
dendhi93 <dendhi.fendi@gmail.com>
2022-08-10 16:41:35 +0700

    1. design login ui

--- app/src/main/java/com/dracoo/medicinemanagement/menus/login/LoginActivity.kt
+++ app/src/main/java/com/dracoo/medicinemanagement/menus/login/LoginActivity.kt
@@ -3,13 +3,16 @@ package com.dracoo.medicinemanagement.menus.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dracoo.medicinemanagement.R
+import com.dracoo.medicinemanagement.databinding.ActivityLoginBinding
+import com.dracoo.medicinemanagement.databinding.ActivitySplashBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: LoginActivity
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
     }
 }
