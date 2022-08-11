c09958d10c5134c6ce4a6c5fff03101ecbe07540
dendhi93 <dendhi.fendi@gmail.com>
2022-08-11 16:51:39 +0700

    1. add menus for add medicine

--- app/src/main/java/com/dracoo/medicinemanagement/menus/new_medicine/NewMedicineActivity.kt
+++ app/src/main/java/com/dracoo/medicinemanagement/menus/new_medicine/NewMedicineActivity.kt
@@ -1,12 +1,15 @@
 package com.dracoo.medicinemanagement.menus.new_medicine
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.dracoo.medicinemanagement.R
 import com.dracoo.medicinemanagement.databinding.ActivityNewMedicineBinding
 import com.dracoo.medicinemanagement.menus.main.view.MainActivity
+import timber.log.Timber
+
 
 class NewMedicineActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewMedicineBinding
@@ -22,11 +25,20 @@ class NewMedicineActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
+                return true
+            }
+            R.id.menu_item ->{
+                Timber.e("coba click")
                 return true
             }
             else -> super.onOptionsItemSelected(item)
